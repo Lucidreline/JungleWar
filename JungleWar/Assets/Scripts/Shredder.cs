1a60348204e2d7b0d3a7133d671389db21d72653
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-04-10 15:17:30 -0700

    The player can now shoot his bullets. You can change the fire rate as well as the velocity of the bullets. After the bullets leave the camera, They will be destroyed.

---
+++ JungleWar/Assets/Scripts/Shredder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+    //will destroy the bullets that leave the camera frame
+
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Destroy(collider.gameObject);
+    }
+}
--- JungleWar/Assets/Scripts/player.cs
+++ JungleWar/Assets/Scripts/player.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-
+    //configs params
     [SerializeField] float moveSpeed = 10;
     [SerializeField] float padding = 0f;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
 
     float xMin;
     float xMax;
@@ -20,20 +25,41 @@ public class player : MonoBehaviour
         setUpMoveBoundries();
     }
 
-    private void setUpMoveBoundries()
-    {
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
-
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+           firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    
+
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject bullet =
+                Instantiate(bulletPrefab, transform.position, Quaternion.identity)
+                as GameObject;
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+
+        }
+
     }
 
     private void Move()
@@ -45,5 +71,15 @@ public class player : MonoBehaviour
         var newYPos = Mathf.Clamp(transform.position.y + deltaY,yMin, yMax) ;
         transform.position = new Vector2(newXPos, newYPos);
         
+    }
+
+    private void setUpMoveBoundries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
     }
 }
