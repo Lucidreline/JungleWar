a199adb331d2727a3a1b059301d3f8f54c252569
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-04-13 20:30:18 -0700

    Player bullets now do a certain ammount of damage to the enemies. Ones the health of the enemy falls to 0 or below 0, the enemy will be destroyed. You can control both the health of each type of enemy aswell as the ammount of damage your bullet will deal.

---
+++ JungleWar/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        health -= damageDealer.GetDamage();
+        ProcessHit(damageDealer);
+        Destroy(other.gameObject);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
